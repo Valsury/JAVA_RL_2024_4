239fdb3df4e045c9d4c73c35fd98d0bdf1017cc4
Valsury <valsury@mail.ru>
2024-11-12 13:26:43 +0300

    Add implementation of subsequent classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,5 +15,45 @@ public class Main {
         System.out.println("Год: " + specificDate.getYear());
         System.out.println("Месяц: " + (specificDate.getMonth() + 1)); // +1, так как месяцы начинаются с 0
         System.out.println("День: " + specificDate.getDay());
+
+        System.out.println("\n");
+
+        BigInteger start = BigInteger.TEN.pow(49);
+        BigInteger end = BigInteger.TEN.pow(50);
+
+
+        BigInteger two = BigInteger.valueOf(2);
+        BigInteger three = BigInteger.valueOf(3);
+
+
+        int count = 0;
+
+
+        for (BigInteger i = start; i.compareTo(end) < 0; i = i.add(BigInteger.ONE)) {
+
+            if (i.mod(two).equals(BigInteger.ZERO) || i.mod(three).equals(BigInteger.ZERO)) {
+                System.out.println(i);
+                count++;
+
+                if (count == 10) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\n");
+        BigInteger maxLongValue = BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger sqrtMaxLongValue = maxLongValue.sqrt();
+
+
+        BigInteger current = sqrtMaxLongValue.add(BigInteger.ONE);
+        int countsquare = 0;
+
+        while (countsquare < 10) {
+            BigInteger square = current.pow(2);
+            System.out.println(square);
+            current = current.add(BigInteger.ONE);
+            countsquare++;
+        }
     }
 }
